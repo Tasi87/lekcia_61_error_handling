46699938f9193157dc17c9cce44bd12df94e4caa
Tasi <tasi87@gmail.com>
2026-04-24 15:09:37 +0200

    Syntax a reference error

--- src/script.js
+++ src/script.js
@@ -1,13 +1,15 @@
-// Error handling
+//idea Error handling
 //! Toto nevyhodí chybu - len vypíše objekt
 
-const error1 = new Error("Chyba!!!")
-console.error(error1)
+const error = new Error ("Chyba!!!")
+console.log(error.name)
+console.log(error.message)
+console.log(error.stack)
 
 // Chyba sa musí vyhodiť (throw) 
 // Akonáhle sa spustí chyba, tak sa zastaví čístanie kódu
 
-throw new Error("Chyba 2!!!");
+// throw new Error("Chyba 2!!!");
 
 // Čo je to throw?
 // throw "Tasi"
@@ -15,4 +17,12 @@ throw new Error("Chyba 2!!!");
 // throw Error
 // throw new Error("");
 
-console.log(5 + 100)
+// console.log(5 + 100)
+
+//note SyntaxError
+// const error1 = !,;
+// throw new SyntaxError();
+
+//note ReferenceError
+// console.log(premmena)
+// throw new ReferenceError();
