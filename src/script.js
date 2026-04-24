1ec3c4b64092509091c549ba45fe9029fb9ba2cb
Tasi <tasi87@gmail.com>
2026-04-24 14:08:00 +0200

    Error handling

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
 // Error handling
+//! Toto nevyhodí chybu - len vypíše objekt
+
+const error1 = new Error("Chyba!!!")
+console.error(error1)
+
+// Chyba sa musí vyhodiť (throw) 
+// Akonáhle sa spustí chyba, tak sa zastaví čístanie kódu
+
+throw new Error("Chyba 2!!!");
+
+// Čo je to throw?
+// throw "Tasi"
+// throw true
+// throw Error
+// throw new Error("");
+
+console.log(5 + 100)
